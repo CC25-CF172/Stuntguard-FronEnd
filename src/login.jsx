d4a4e430cde5330b0f204c4d0169a0dea683b921
Isnan <kusumaisnan9@gmail.com>
2026-08-03 11:00:13 +0700

    merubah ke ENG pada sign in dan signin

--- src/login.jsx
+++ src/login.jsx
@@ -265,7 +265,7 @@ export default function StuntWatchLogin() {
               className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#0284c7] hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out disabled:opacity-50"
             >
               <span className="mr-2">→</span>
-              {loading ? 'Signing in...' : 'Sign in'}
+              {loading ? 'Sedang Masuk...' : 'Masuk'}
             </button>
           </div>
 
